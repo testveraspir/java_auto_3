5f4bf80e6c191fdc199c3a6cb494f81ccad51c96
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-05-18 12:42:39 +0300

    new tests

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -1,7 +1,6 @@
 package ru.netology.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static io.github.bonigarcia.wdm.config.DriverManagerType.CHROME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
@@ -17,24 +15,23 @@ public class CallbackTest {
     private WebDriver driver;
 
 
+    /**
+     * @BeforeAll static void setUpAll() {
+     * System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+     * <p>
+     * }
+     */
 
- /** @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
 
-    }*/
-
-
-
-   @BeforeEach
+    @BeforeEach
     void setUp() {
-           WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
 
-     driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
 
     }
@@ -57,4 +54,37 @@ public class CallbackTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
     }
+
+    @Test
+    void invalidFieldName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Petrov Ivan");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79017865423");
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String text = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void invalidFieldPhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("79017865423");
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String text = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void invalidFieldCheckBox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79017865423");
+        driver.findElement(By.cssSelector(".button")).click();
+        String text = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
+
 }
